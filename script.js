a27f9b97a074195568b62ccba1e186e5207dade0
Awilistrators <amar281299@gmail.com>
2026-05-19 17:32:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API="https://script.google.com/macros/s/AKfycbxDlr7d5EJ_wWXFlKhmts73-n-XNvxguoL8CddeAz0E2zIY2Yrj7zld9Jum4pBKsvVuxQ/exec";
+const API="https://script.google.com/macros/s/AKfycbwkXaK8GDt8yXHNEKKo1_7A2S4nLt6nghB5EYIyzpNqrt3usv7w3KW0tRgdg8enOFyJUQ/exec";
 
 let MASTER=[];
 let produk=null;
